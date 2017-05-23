c9763df2a91dbec942367dd457eeaf1f69a5f930
Kathy Chiu <chiuy5@uw.edu>
2017-05-23 14:41:47 -0700

    made updates

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -15,15 +15,16 @@ book.df <- austen_books()
 book.df %>% select(book) %>% unique() %>% nrow()
 
 # Which book has the most lines?
-book.frq <- book.df %>% select(book) %>% table() %>% data.frame() 
-colnames(book.frq) <- c("book", "Freq")
-book.frq %>% filter(Freq == max(Freq)) %>% select(book)
+#book.frq <- book.df %>% select(book) %>% table() %>% data.frame() 
+#colnames(book.frq) <- c("book", "Freq")
+#book.frq %>% filter(Freq == max(Freq)) %>% select(book)
+book.lines <- book.df %>% group_by(book) %>% summarise(line = n()) %>% filter(line == max(line)) %>% select(book)
 
 # Use the unnest_tokens function to generate the full list of words
-
+all.words <- book.df %>% unnest_tokens(word, text)
 
 # Which words are most common (regardless of which book them come from)?
-
+word.count <- all.words %>% group_by(word) %>% summarise(count = n()) %>% arrange(-count)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
 
